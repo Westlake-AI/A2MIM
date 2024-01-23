939bc3d2e263b287f0aecdca82d9c29e4fb28c33
Lupin1998 <1070535169@qq.com>
2024-01-23 01:28:12 +0000

    update a2mim release

--- configs/openmixup/pretrain/a2mim/imagenet/r200_l3_sz224_init_8xb256_cos_ep300.py
+++ configs/openmixup/pretrain/a2mim/imagenet/r200_l3_sz224_init_8xb256_cos_ep300.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../../_base_/models/a2mim/moganet_s.py',
+    '../../_base_/models/a2mim/r200.py',
     '../../_base_/datasets/imagenet/a2mim_rgb_m_sz224_rrc08_bs64.py',
     '../../_base_/default_runtime.py',
 ]
@@ -9,11 +9,7 @@ model = dict(
     backbone=dict(
         mask_layer=3, mask_token="learnable",
         mask_init=1e-6,  # init residual gamma
-    ),
-    head=dict(
-        fft_weight=0., fft_focal=False,
-    ),
-)
+))
 
 # dataset
 data = dict(
@@ -22,36 +18,34 @@ data = dict(
         feature_mode=None, feature_args=dict(),
         mask_pipeline=[
             dict(type='BlockwiseMaskGenerator',
-                input_size=224, mask_patch_size=32, mask_ratio=0.6, model_patch_size=32,  # stage 3
+                input_size=224, mask_patch_size=32, mask_ratio=0.6, model_patch_size=16,  # stage 3
                 mask_color='mean', mask_only=False),
         ],
 ))
 
 # interval for accumulate gradient
-update_interval = 1  # bs256 x 8gpus = bs2048
+update_interval = 1
 
 # additional hooks
 custom_hooks = [
     dict(type='SAVEHook',
-        save_interval=626 * 25,  # plot every 25 ep
+        save_interval=626 * 10,  # plot every 10 ep
         iter_per_epoch=626),
 ]
 
 # optimizer
 optimizer = dict(
     type='AdamW',
-    lr=3e-4 * 2048 / 512,  # 3e-4 * 4 for bs2048
+    lr=3e-4 * 2048 / 512,  # 1.2e-3 for bs2048
     betas=(0.9, 0.999), weight_decay=0.05, eps=1e-8,
     paramwise_options={
         '(bn|ln|gn)(\d+)?.(weight|bias)': dict(weight_decay=0.),
         'bias': dict(weight_decay=0.),
-        'gamma': dict(weight_decay=0.),
         'mask_token': dict(weight_decay=0., lr_mult=1e-1,),
-        'mask_gamma': dict(weight_decay=0., lr_mult=1e-1,),
     })
 
 # fp16
-use_fp16 = True
+use_fp16 = False
 fp16 = dict(type='mmcv', loss_scale='dynamic')
 # optimizer args
 optimizer_config = dict(update_interval=update_interval)
--- configs/openmixup/pretrain/a2mim/imagenet/vit_small_l0_sz224_8xb256_step_ep800.py
+++ configs/openmixup/pretrain/a2mim/imagenet/vit_small_l0_sz224_8xb256_step_ep800.py
@@ -1,34 +1,29 @@
 _base_ = [
-    '../../_base_/models/a2mim/moganet_s.py',
-    '../../_base_/datasets/imagenet/a2mim_rgb_m_sz224_rrc08_bs64.py',
+    '../../_base_/models/a2mim/vit_small.py',
+    '../../_base_/datasets/imagenet/a2mim_rgb_m_sz224_bs64.py',
     '../../_base_/default_runtime.py',
 ]
 
 # model settings
 model = dict(
     backbone=dict(
-        mask_layer=3, mask_token="learnable",
-        mask_init=1e-6,  # init residual gamma
-    ),
-    head=dict(
-        fft_weight=0., fft_focal=False,
-    ),
+        mask_layer=0, mask_token='learnable')
 )
 
 # dataset
 data = dict(
-    imgs_per_gpu=256, workers_per_gpu=10,
+    imgs_per_gpu=256, workers_per_gpu=12,
     train=dict(
         feature_mode=None, feature_args=dict(),
         mask_pipeline=[
             dict(type='BlockwiseMaskGenerator',
-                input_size=224, mask_patch_size=32, mask_ratio=0.6, model_patch_size=32,  # stage 3
+                input_size=224, mask_patch_size=32, model_patch_size=16, mask_ratio=0.6,
                 mask_color='mean', mask_only=False),
         ],
 ))
 
 # interval for accumulate gradient
-update_interval = 1  # bs256 x 8gpus = bs2048
+update_interval = 1  # total: 8 x bs256 x 1 accumulates = bs2048
 
 # additional hooks
 custom_hooks = [
@@ -40,30 +35,34 @@ custom_hooks = [
 # optimizer
 optimizer = dict(
     type='AdamW',
-    lr=3e-4 * 2048 / 512,  # 3e-4 * 4 for bs2048
+    lr=1e-4 * 2048 / 512,  # 4e-4 for bs2048
     betas=(0.9, 0.999), weight_decay=0.05, eps=1e-8,
     paramwise_options={
         '(bn|ln|gn)(\d+)?.(weight|bias)': dict(weight_decay=0.),
+        'norm': dict(weight_decay=0.),
         'bias': dict(weight_decay=0.),
+        'mask_token': dict(weight_decay=0.),
+        'pos_embed': dict(weight_decay=0.),
+        'cls_token': dict(weight_decay=0.),
         'gamma': dict(weight_decay=0.),
-        'mask_token': dict(weight_decay=0., lr_mult=1e-1,),
-        'mask_gamma': dict(weight_decay=0., lr_mult=1e-1,),
     })
 
 # fp16
-use_fp16 = True
+use_fp16 = False
 fp16 = dict(type='mmcv', loss_scale='dynamic')
 # optimizer args
-optimizer_config = dict(update_interval=update_interval)
+optimizer_config = dict(
+    update_interval=update_interval,
+    grad_clip=dict(max_norm=5.0),
+)
 
 # lr scheduler
 lr_config = dict(
-    policy='StepFixCosineAnnealing',
-    by_epoch=False, min_lr=1e-5,
+    policy='step', step=[700,], gamma=0.1,
     warmup='linear',
     warmup_iters=10, warmup_by_epoch=True,
-    warmup_ratio=1e-6,
+    warmup_ratio=5e-7 * 2048 / 512,
 )
 
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=300)
+runner = dict(type='EpochBasedRunner', max_epochs=800)
